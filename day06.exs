560889a1623305ebcaeffdac41a3ee80cd532c06
Ismael Haddad <ismael.haddad@prototyp.se>
2021-12-07 11:07:34 +0100

    day06 part 2

--- day06.exs
+++ day06.exs
@@ -7,43 +7,88 @@ defmodule MySlickTools do
 
   def read_list_of_integers(filename) do
     {:ok, contents} = File.read(filename)
+
     contents
-     |> String.split(",", trim: true)
-     |> Enum.map(&(Integer.parse(&1) |> Kernel.elem(0)))
+    |> String.split(",", trim: true)
+    |> Enum.map(&(Integer.parse(&1) |> Kernel.elem(0)))
   end
 end
 
-defmodule LanternfishSimulator do
-  def total_lanternfish_in_n_days(list, days) do
-    Enum.reduce(list, 0, fn lanternfish, sum -> sum + recursive_count(lanternfish, days) end)
+defmodule Lanternfish do
+  use Agent
+
+  def start do
+    Agent.start_link(fn -> %{} end, name: __MODULE__)
   end
 
-  defp recursive_count(days_cycle, days) do
+  def how_many_fishes_in_n_days(9, days) do
+    cached_value = Agent.get(__MODULE__, &Map.get(&1, days))
+
+    if cached_value do
+      cached_value
+    else
+      cond do
+        9 < days ->
+          num_of_lanternfishes_to_have = Kernel.ceil((days - 9) / 7)
+
+          v =
+            Enum.reduce(0..num_of_lanternfishes_to_have, 0, fn index, sum ->
+              sum + how_many_fishes_in_n_days(9, days - 9 - index * 7)
+            end)
+
+          Agent.update(__MODULE__, &Map.put(&1, days, v))
+          v
+
+        true ->
+          Agent.update(__MODULE__, &Map.put(&1, days, 1))
+          1
+      end
+    end
+  end
+
+  def how_many_fishes_in_n_days(days_cycle, days) do
     cond do
       days_cycle < days ->
         num_of_lanternfishes_to_have = Kernel.ceil((days - days_cycle) / 7)
+
         Enum.reduce(0..num_of_lanternfishes_to_have, 0, fn index, sum ->
-          sum + recursive_count(9, (days - days_cycle) - index * 7)
+          sum + how_many_fishes_in_n_days(9, days - days_cycle - index * 7)
         end)
-      true -> 1
+
+      true ->
+        1
     end
   end
 end
 
+defmodule LanternfishSimulator do
+  def start do
+    {:ok, _} = Lanternfish.start()
+  end
+
+  def total_lanternfish_in_n_days(list, days) do
+    Enum.reduce(list, 0, fn lanternfish, sum ->
+      sum + Lanternfish.how_many_fishes_in_n_days(lanternfish, days)
+    end)
+  end
+end
 
 ##
 # MAIN ENTRY SCRIPT
 #
 
-list = MySlickTools.get_filename_from_arg()
- |> MySlickTools.read_list_of_integers()
+list =
+  MySlickTools.get_filename_from_arg()
+  |> MySlickTools.read_list_of_integers()
 
-IO.puts "Part one"
+IO.puts("Part one")
+
+LanternfishSimulator.start()
 
 LanternfishSimulator.total_lanternfish_in_n_days(list, 80)
-|> IO.inspect
+|> IO.inspect()
 
-# IO.puts "Part two"
+IO.puts("Part two")
 
-# LanternfishSimulator.total_lanternfish_in_n_days(list, 256)
-# |> IO.inspect
+LanternfishSimulator.total_lanternfish_in_n_days(list, 256)
+|> IO.inspect()
