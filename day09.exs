c425d419f650c7d7e3f86e254331d26d8c771dc3
Ismael Haddad <ismael.haddad@prototyp.se>
2021-12-10 18:07:05 +0100

    day09 part2

--- day09.exs
+++ day09.exs
@@ -52,6 +52,47 @@ defmodule Heightmap do
       end
     end)
   end
+
+  ## PART TWO
+
+  def calculate_top_3_largest_basins({matrix_map, _, _}) do
+    Enum.reduce(matrix_map, {[], []}, fn {pos, _}, {visited, sizes} ->
+      cur_val = Map.get(matrix_map, pos)
+
+      if cur_val == 9 or Enum.member?(visited, pos) do
+        {[pos | visited], sizes}
+      else
+        {rec_visited, size} = get_basin_size(matrix_map, pos, visited)
+        {rec_visited, [size - 1 | sizes]}
+      end
+    end)
+    |> Kernel.elem(1)
+    |> Enum.sort(&(&1 >= &2))
+  end
+
+  defp get_basin_size(matrix_map, pos, visited) do
+    get_adjecents_positions(pos)
+    |> Enum.reduce({visited, 1}, fn cur_pos, {acc_visited, sum} ->
+      if not Enum.member?(acc_visited, cur_pos) do
+        case Map.fetch(matrix_map, cur_pos) do
+          {:ok, value} ->
+            if value != 9 do
+              {rec_visited, rec_sum} =
+                get_basin_size(matrix_map, cur_pos, [cur_pos | acc_visited])
+
+              {rec_visited, rec_sum + sum}
+            else
+              {[cur_pos | acc_visited], sum}
+            end
+
+          :error ->
+            {[cur_pos | acc_visited], sum}
+        end
+      else
+        {[cur_pos | acc_visited], sum}
+      end
+    end)
+  end
 end
 
 matrix =
@@ -62,3 +103,8 @@ IO.puts("Part one")
 
 Heightmap.calculate_risk_points(matrix)
 |> IO.inspect()
+
+IO.puts("Part two")
+
+[top1 | [top2 | [top3 | _]]] = Heightmap.calculate_top_3_largest_basins(matrix)
+IO.inspect({{top1, top2, top3}, top1 * top2 * top3})
